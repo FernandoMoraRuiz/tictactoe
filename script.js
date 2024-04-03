2ca7dadaf8aed0f4a179ac78f1e2505e08c4b35e
Fernando Mora Ruiz <147545594+FernandoMoraRuiz@users.noreply.github.com>
2024-04-03 13:00:16 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let playerOne = true;
+    btn = document.querySelectorAll(".btn")
+
+btn.forEach(key => {
+    key.addEventListener("click", playerMove);
+});
+
+function playerMove(e) {
+    writeMove(e)
+};
+
+function writeMove(e) {
+    if(e.target.className === "btn" && playerOne === true) {
+        e.target.classList.add("x");
+        playerOne = !playerOne;
+    }
+    else {
+        e.target.classList.add("o");
+        playerOne = !playerOne;
+    }
+    check(0,1,2);
+    check(3,4,5);
+    check(6,7,8);
+    check(0,3,6);
+    check(1,4,7);
+    check(2,5,8);
+    check(0,4,8);
+    check(2,4,6);
+}
+
+function check(btn1,btn2,btn3) {
+    if (
+        btn[btn1].className === "btn x" &&
+        btn[btn2].className === "btn x" &&
+        btn[btn3].className === "btn x"
+        ) {
+            window.alert("x win !")
+
+        }
+    else if (
+        btn[btn1].className === "btn o" &&
+        btn[btn2].className === "btn o" &&
+        btn[btn3].className === "btn o"
+        ) {
+
+            window.alert("o win !")
+
+        } 
+}
